aa973b74214b0ddb5d8fd020daa68827db02245e
brian.h <anhkun123456@gmail.com>
2023-09-23 23:07:07 +0700

    remove package

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { PrimeReactProvider } from "primereact/api";
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-//core
-import "primereact/resources/primereact.min.css";
-import "/node_modules/primeflex/primeflex.css";
 import GlobalContextProvider from "./contexts/globalContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <GlobalContextProvider>
-        <App />
-      </GlobalContextProvider>
-    </PrimeReactProvider>
+    <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>
   </React.StrictMode>
 );
